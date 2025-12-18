c2b96dc106023835b8a2bfd08a4700076a8ba4eb
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:27:29 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MICHEL TELÓ';
+export let nameSongs = [
+	"AI SE EU TE PEGO", /*1*/
+	"BARÁ BERÊ", /*2*/
+	"FUGIDINHA", /*3*/
+	"LARGA DE BOBEIRA", /*4*/
+	"AMIGA DA MINHA IRMÃ", /*5*/
+	"HUMILDE RESIDENCIA", /*6*/
+	"EI PSIU, BEIJO ME LIGA", /*7*/
+	"LEVEMENTE ALTERADO", /*8*/
+	"MOTIVOS", /*9*/
+	"CHICO MINEIRO", /*10*/
+	"ESTOU APAIXONADO", /*11*/
+	"ÍNDIA", /*12*/
+	"DOLCE GABBANA", /*13*/
+	"O MAR PAROU", /*14*/
+	"CORAÇÃOZINHO ORGULHOSO", /*15*/
+	"MULEKE OU HOMEM", /*16*/
+	"TE AMO SONHANDO", /*17*/
+	"COMO É QUE FAZ", /*18*/
+	"MINTO BEM DEMAIS", /*19*/
+	"CASAL MODÃO", /*20*/
+]
